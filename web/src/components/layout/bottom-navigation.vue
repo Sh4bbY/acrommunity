b351e65907c62d6b716eb3cd73cd4bd3783d3ff7
Sascha Bialon <development@shabbtech.com>
2022-11-25 21:13:08 +0100

    Update: add basic auth protection

--- web/src/components/layout/bottom-navigation.vue
+++ web/src/components/layout/bottom-navigation.vue
@@ -16,8 +16,8 @@ export default class AppBottomNavigation extends Vue {
   get items() {
     return [
       // {title: this.$t('label.home'), icon: 'mdi-home', route: {name: 'home'}, exact: true},
-      {title: this.$tc('p.app', 2), icon: 'mdi-gamepad-variant', route: {name: 'apps'}},
       {title: this.$t('label.dictionary'), icon: 'mdi-book-open-variant', route: {name: 'dictionary'}},
+      {title: this.$tc('p.app', 2), icon: 'mdi-gamepad-variant', route: {name: 'apps'}},
       {title: this.$tc('p.jam', 2), icon: 'mdi-calendar', route: {name: 'jams'}},
     ];
   }
